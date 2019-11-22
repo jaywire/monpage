e498c4f3ae4942e2b5522c3c3710c816b59fd46b
jaywire <ritzjoua08@gmail.com>
2019-11-21 20:24:49 -0500

    Update monpage.php

--- monpage.php
+++ monpage.php
@@ -1,35 +1,36 @@
 #!/usr/bin/php
-
 <?php
 /*
 */
-
 
 require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-echo "Page we're watching: ";
-$page = rtrim(fgets(STDIN));
-$data = 'datatest/';
-
 $client = new Client([
-        'timeout'  => 2.0,
+    'timeout'  => 2.0,
 ]);
+
+echo "Page we're watching: ";
+$url = rtrim(fgets(STDIN));
+
+//Defining variables
+$data = 'datatest/';
+$response = $client->get($url);
+$body = $response->getBody();
+$content = $body->getContents();
 
 if (!file_exists($data)) {
     echo "\n*** ERROR *** ||| $data not found, created directory\n";
     mkdir($data, 0777, true);
-} 
+}
 
-$url = $page;
-$response = $client->get($url);
-$body = $response->getBody();
-$content = $body->getContents();
 $sha = sha1($content);
 if(!file_exists("$data/$sha")) {
     file_put_contents("$data/$sha", $content);
+
 }
+
 $body->getSize();
 
 $scanFile1 = scandir($data);
